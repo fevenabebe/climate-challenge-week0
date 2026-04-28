1cf6549feebed73ae235c53dbe31dfe75c478855
fevenabebe <fevenabebe777@gmail.com>
2026-04-28 19:40:19 +0300

    fix file location

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from utils import load_data, filter_data
+from app.utils import load_data, filter_data
 
 # -----------------------------
 # PAGE CONFIG
